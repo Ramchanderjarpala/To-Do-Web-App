916bae8ddf541abeba2c755795469ded1a16cabe
Ramchanderjarpala <ramchander010477@gmail.com>
2025-05-10 17:11:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Request logging middleware
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
