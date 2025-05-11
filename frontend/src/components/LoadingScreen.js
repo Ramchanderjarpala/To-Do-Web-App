d6217a4eaf1228ab21f3c131268c682df2c5a221
Ramchanderjarpala <ramchander010477@gmail.com>
2025-05-11 12:59:42 +0530

    Update LoadingScreen.js

--- frontend/src/components/LoadingScreen.js
+++ frontend/src/components/LoadingScreen.js
@@ -4,7 +4,7 @@ import { SyncLoader } from 'react-spinners';
 export default function LoadingScreen() {
   return (
     <div className="fixed inset-0 flex flex-col justify-center items-center bg-gray-900 bg-opacity-90 z-50">
-      <SyncLoader color="white" size={40} />
+      <SyncLoader color="white" size={20} />
       {/* <h2 className="text-center text-white text-xl font-semibold mt-4">Loading tasks...</h2> */}
     </div>
   );
